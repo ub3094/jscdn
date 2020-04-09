076cd1280a05eedb14d041a8c5cd38b926e40bdc
ub3094 <63275042+ub3094@users.noreply.github.com>
2020-04-09 20:57:53 +0800

    Create sp.js

---
+++ ads/sp.js
@@ -0,0 +1,2 @@
+document.writeln("<a href=\"http:\/\/www.99sp.pw\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fulidhw\/cdn\/img\/gdsp.gif\"\/><\/a>
+         <a href=\"http:\/\/www.99sp.pw\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fulidhw\/cdn\/img\/gdsp.gif\"\/><\/a>");
